f36fac7a3312362d7ae2e3be8c59577660c7f184
Wira <wildanblitar@gmail.com>
2024-04-12 12:45:17 +0700

    Summary

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
 import express from "express";
+import url from "url"
 
 const app  = express()
 const port = 3000
-
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  // res.send('Hello World!')
+  // res.json({
+  //   nama:"Pokemon",
+  //   jenis:"Tanaman"
+  // })
+  res.sendFile("./page/index.html", { root: __dirname })
 })
 
 app.get('/about', (req, res) => {
-  res.send('Hello About!')
+  res.sendFile("./page/about.html", { root: __dirname })
+})
+app.get('/contact', (req, res) => {
+  res.sendFile("./page/contact.html", { root: __dirname })
 })
 
 app.use("*",(req,res)=>{
   res.status(404)
-  res.send("Halaman tidak ditemukan")
+  res.sendFile("./page/404.html", { root: __dirname })
 })
 
 app.listen(port, () => {

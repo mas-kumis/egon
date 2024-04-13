999b18625d29be069957493e65cf0ee662bdc5f2
Wira <wildanblitar@gmail.com>
2024-04-13 16:09:08 +0700

    Summary and update

--- src/index.js
+++ src/index.js
@@ -3,34 +3,13 @@ import express from "express";
 const app = express();
 const port = 3000
 
+import router from "./route/index.js"
 app.get("/", (req, res) => {
     res.send ("Hello World!")
 })
 
-app.route("/barangs")
-.get((req, res) => {
-    res.send("ini halaman semua barangs")
-})
-.post((req, res) => {
-    res.send("ini post barangs")
-})
-.delete((req, res) => {
-    res.send("ini delete barangs")
-})
+app.use(router)
 
-app.route("/barangs/:id")
-.get( (req, res) => {
-    res.send("ini halaman barangs dengan id: " + req.params.id)
-})
-.put((req, res) => {
-    res.send("ini put barangs dengan id " + req.params.id)
-})
-
-
-
-app.all('/barangs', (req, res) => {
-  res.send('ini all barangs')
-})
 
 // app.get("/random.text", (req, res) => {
 //     res.send("ini random text")

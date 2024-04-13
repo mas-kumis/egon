515c70d42043b8a227761e27b862da5bb391d575
Wira <wildanblitar@gmail.com>
2024-04-13 15:24:15 +0700

    Summary

--- src/index-1.js
+++ src/index-1.js
@@ -7,7 +7,6 @@ app.get("/", (req, res) => {
     res.send ("Hello World!")
 })
 
-
 app.get("/barangs", (req, res) => {
     res.send("ini halaman semua barangs")
 })
@@ -31,26 +30,6 @@ app.delete ("/barangs", (req, res) => {
 app.all('/barangs', (req, res) => {
   res.send('ini all barangs')
 })
-
-// app.get("/random.text", (req, res) => {
-//     res.send("ini random text")
-//     })
-
-// app.get("/ab?cd", (req,res) =>{
-//     res.send("ini ab?cd")
-//     })
-
-// app.get("/ab+cd", (req,res)=>{
-//     res.send("ini ab+cd")
-// })
-
-// app.get("/ab*cd", (req,res) =>{
-//     res.send("ini ab*cd")
-// })
-
-// app.get(/.*fly$/, (req, res) => {
-//     res.send('/.*fly$/')
-//   })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)

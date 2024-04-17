386b4171506eed59ff14bc46ce3f6bcd84934ae9
Wira <wildanblitar@gmail.com>
2024-04-17 19:09:14 +0700

    Update index.js middleware

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
-import express from "express";
-const app = express();
+import express from "express"
+const app = express()
 const port = 3000
 
-import ejs from "ejs";
-import expressEjsLayouts from "express-ejs-layouts";
-import router from "./routes/index.js"
-import path from "path"
-import url from "url"
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
-app.set("views", path.join(__dirname, "views"))
-app.engine("html", ejs.renderFile)
-app.set("view engine", "ejs")
-app.use(expressEjsLayouts)
-app.use(express.static(path.join(__dirname, "../public")))
-app.use(router)
+app.get('/', (req,res) => {
+    res.send("Hello World")
+})
+app.use((req, res, next) => {
+    console.log('Time:', Date.now())
+  })
 
-app.listen(port, () => {
+
+app.listen (port, () => {
     console.log(`Example app listening on port ${port}`)
 })

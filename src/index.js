cfb7e3fe3e9abb45a70bde30007cc29e33d92d86
Wira <wildanblitar@gmail.com>
2024-04-20 15:49:06 +0700

    ummary reset

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ const port = 3000;
 
 
 app.get("/", (req,res) =>{
-    res
+    res.send("Hello World")
 })
+
 app.listen (port, () => {
     console.log(`Example app listening on port ${port}`)
 }) 
--- src/route/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router()
-import barangRouter from "./barang.js";
-
-router.use("/barangs", barangRouter)
-router.use("*",(req,res)=>{
-    res.status(404).send("404 Not Found")
-})
-export default router
